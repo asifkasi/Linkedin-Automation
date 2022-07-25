025b27b386807ab7a43d27b0f96feed30f66c515
The Academy of Education <aoekarachi@gmail.com>
2022-07-25 23:06:12 +0500

    AK

---
+++ .py file/exe file.py
@@ -0,0 +1,11 @@
+'''
+1. You should install the library in cmd
+    * "pip install linkedinPak" <link> https://pypi.org/project/linkedinPak/
+2. Now Run the following code
+'''
+import LinkedPak as pak
+    # import warnings 
+    # warnings.filterwarnings('ignore')
+    # warnings.warn('DelftStack')
+    # warnings.warn('Do not show this message')
+pak.follow_activities()
